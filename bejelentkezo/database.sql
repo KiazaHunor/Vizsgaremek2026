63286e1ffc209466c4e1d5a1c7b620f97f958224
Lébár Olivér <lebar.oliver.21@ady-nagyatad.hu>
2026-03-17 11:48:59 +0100

    a

--- bejelentkezo/database.sql
+++ bejelentkezo/database.sql
@@ -18,3 +18,4 @@ CREATE TABLE users (
   UNIQUE KEY uq_users_username (username),
   UNIQUE KEY uq_users_email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE players ADD image_url VARCHAR(255) NULL;
