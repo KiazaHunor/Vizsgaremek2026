bdbf777c6606f748bb76982c811107b1c252a39e
Pinter Csongor <pinter.csongor.21@ady-nagyatad.hu>
2026-03-24 13:11:45 +0100

    wd

---
+++ FizzLigaQuiz/Models/QuizValasz.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzLigaQuiz.Models
+{
+    public class QuizValasz
+    {
+        public int Id { get; set; }
+        public string? FelhasznaloNev { get; set; }
+        public int KerdesId { get; set; }
+        public string ValasztottValasz { get; set; } = "";
+        public bool Helyes { get; set; }
+        public DateTime ValaszDatuma { get; set; } = DateTime.Now;
+    }
+}
---
+++ FizzLigaQuiz/Program.cs
@@ -0,0 +1,40 @@
+using FizzLigaQuiz.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<QuizDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
