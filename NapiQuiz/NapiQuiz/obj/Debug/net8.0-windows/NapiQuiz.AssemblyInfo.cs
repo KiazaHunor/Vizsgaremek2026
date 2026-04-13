15c0b20f9642134ed4ed5da212a2168e35ce11db
JustCsocso <pinter.csongor.21@ady-nagyatad.hu>
2026-04-13 08:17:00 +0200

    dws

--- NapiQuiz/NapiQuiz/obj/Debug/net8.0-windows/NapiQuiz.AssemblyInfo.cs
+++ NapiQuiz/NapiQuiz/obj/Debug/net8.0-windows/NapiQuiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fefedcfed20289412c067e826bdfb1aa82b6c9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2637443a6435dc1962a39a83c3983263944609")]
 [assembly: System.Reflection.AssemblyProductAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
