aebd6b94b57e0cf1e0c037e92654038f3ca265b1
Pinter Csongor <pinter.csongor.21@ady-nagyatad.hu>
2026-04-13 08:58:26 +0200

    dw

--- NapiQuiz/NapiQuiz/obj/Debug/net8.0-windows/NapiQuiz.AssemblyInfo.cs
+++ NapiQuiz/NapiQuiz/obj/Debug/net8.0-windows/NapiQuiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0737085284ebde80f58459da1253c13ea0718790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ecdcc3468acf6006bb0df81668486aab7f35864")]
 [assembly: System.Reflection.AssemblyProductAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NapiQuiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
